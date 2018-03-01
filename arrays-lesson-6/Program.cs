d7b7203311350bffbabb911dae6a9e7273f27695
Andrey Salnikov <a.salnikov.home@gmail.com>
2018-03-01 20:34:33 +0200

    ARRAY1

--- arrays-lesson-6/Program.cs
+++ arrays-lesson-6/Program.cs
@@ -10,6 +10,85 @@ namespace arrays_lesson_6
     {
         static void Main(string[] args)
         {
-        }
+            //int[] numbers = new int[5];
+            //int[] srebmun = new int[5];
+
+            //int iii = 1;
+            //int qqq = 4;
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    numbers[i] = iii;
+            //    iii++;
+            //}
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.WriteLine(numbers[i]);
+            //}
+            //for (int i = 0; i < srebmun.Length; i++)
+            //{
+            //    srebmun[i] = numbers[qqq];
+            //    qqq--;
+            //}
+            //for (int i = 0; i < srebmun.Length; i++)
+            //{
+            //    Console.WriteLine(srebmun[i]);
+            //}
+
+
+            //string[] daysOfWeek = { "Monday" };
+
+
+            //string[] array1 = { "One", "Two", "Three" };
+            //string[] array2 = new string[3];
+            //string znak = "!";
+
+            //for (int i = 0; i < array1.Length; i++)
+            //{
+            //    array2[i] = array1[i] + znak;
+            //}
+            //for (int i = 0; i < array2.Length; i++)
+            //{
+            //    Console.WriteLine(array2[i]);
+            //}
+
+            //int[,,,] arrayMatrix = new int[4,3,2,5];
+            //Random random = new Random();
+            //int t = random.Next(10,21);
+
+            //for (int i = 0; i < arrayMatrix.GetLength(0); i++)
+            //{
+            //    for (int ii = 0; ii < arrayMatrix.GetLength(1); ii++)
+            //    {
+            //        for (int iii = 0; iii < arrayMatrix.GetLength(2); iii++)
+            //        {
+            //            for (int iiii = 0; iiii < arrayMatrix.GetLength(3); iiii++)
+            //            {
+            //                arrayMatrix[i, ii, iii, iiii] = random.Next(0, 10);
+            //            }
+            //        }
+                    
+            //    }
+            //}
+            //for (int q = 0; q < arrayMatrix.GetLength(0); q++)
+            //{
+            //    for (int w = 0; w < arrayMatrix.GetLength(1); w++)
+            //    {
+            //        for (int e = 0; e < arrayMatrix.GetLength(2); e++)
+            //        {
+            //            for (int r = 0; r < arrayMatrix.GetLength(3); r++)
+            //            {
+            //                Console.Write($"{arrayMatrix[q, w, e, r]} \t");
+            //            }
+            //        }
+                                     
+            //    }
+            //    Console.WriteLine();
+            //}  
+
+
+
+
+
+            }
     }
 }
